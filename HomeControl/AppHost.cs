f2690dd81ff16a51670bf083b1684be88b1ae253
Sebastian Kromp <sebastian.kromp@googlemail.com>
2015-12-15 09:08:29 +0100

    - added first (very simple) logging implementation - added authentication

--- HomeControl/AppHost.cs
+++ HomeControl/AppHost.cs
@@ -1,11 +1,15 @@
 ﻿using HomeControl.Data;
 using HomeControl.Data.Interfaces;
 using HomeControl.DatabaseServices;
+using HomeControl.LoggingAdapter;
 using HomeControl.Services;
 using Ninject;
 using Ninject.Activation;
 using Serilog;
 using ServiceStack;
+using ServiceStack.Auth;
+using ServiceStack.Caching;
+using ServiceStack.Logging;
 
 namespace HomeControl
 {
@@ -19,7 +23,22 @@ namespace HomeControl
         {
             IKernel kernel = new StandardKernel();
             RegisterServices(kernel);
+
             container.Adapter = new NinjectIocAdapter(kernel);
+
+            LogManager.LogFactory = container.Resolve<ILogFactory>();
+
+            Plugins.Add(new AuthFeature(() => new AuthUserSession(),
+                    new IAuthProvider[] {
+                    new BasicAuthProvider(), //Sign-in with HTTP Basic Auth
+                    new CredentialsAuthProvider(), //HTML Form post of UserName/Password credentials
+                  }));
+
+            Plugins.Add(new RegistrationFeature());
+
+            container.Register<ICacheClient>(new MemoryCacheClient());
+            var userRep = new InMemoryAuthRepository();
+            container.Register<IUserAuthRepository>(userRep);
         }
 
         private void RegisterServices(IKernel kernel)
@@ -30,17 +49,14 @@ namespace HomeControl
             kernel.Bind<IUserDatabaseService>().To<UserDatabaseService>();
             kernel.Bind<IAuthenticationService>().To<AuthenticationService>();
             kernel.Bind<IDatabaseContextFactory>().To<DatabaseContextFactory>();
+            kernel.Bind<ILogFactory>().To<SerilogFactory>();
+            kernel.Bind<ILog>().To<SeriLogAdapter>();
         }
 
         private ILogger CreateContextLogger(IContext context)
         {
             var requestingType = context.Request?.ParentRequest?.ParentRequest?.Target?.Member?.DeclaringType;
-            if (requestingType == null)
-            {
-                return Log.Logger;
-            }
-
-            return Log.Logger.ForContext(requestingType);
+            return requestingType == null ? Log.Logger : Log.Logger.ForContext(requestingType);
         }
 
         private void InitializeLogging()
---
+++ HomeControl/LoggingAdapter/SeriLogAdapter.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Serilog;
+using ServiceStack.Logging;
+
+namespace HomeControl.LoggingAdapter
+{
+    class SeriLogAdapter : ILog
+    {
+        private readonly ILogger _logger;
+
+        public SeriLogAdapter(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public bool IsDebugEnabled
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Debug(object message)
+        {
+            _logger.Debug(message.ToString());
+        }
+
+        public void Debug(object message, Exception exception)
+        {
+            _logger.Debug(message.ToString());
+        }
+
+        public void DebugFormat(string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Error(object message)
+        {
+            _logger.Error(message.ToString());
+        }
+
+        public void Error(object message, Exception exception)
+        {
+            _logger.Error(message.ToString());
+        }
+
+        public void ErrorFormat(string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Fatal(object message)
+        {
+            _logger.Fatal(message.ToString());
+        }
+
+        public void Fatal(object message, Exception exception)
+        {
+            _logger.Fatal(message.ToString());
+        }
+
+        public void FatalFormat(string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Info(object message)
+        {
+            _logger.Information(message.ToString());
+        }
+
+        public void Info(object message, Exception exception)
+        {
+            _logger.Information(message.ToString());
+        }
+
+        public void InfoFormat(string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Warn(object message)
+        {
+            _logger.Warning(message.ToString());
+        }
+
+        public void Warn(object message, Exception exception)
+        {
+            _logger.Warning(message.ToString());
+        }
+
+        public void WarnFormat(string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
